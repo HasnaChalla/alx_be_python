0a4008cc876185b909435643380402e7a06913f7
Hasna_challa <challahasna@gmail.com>
2025-09-20 21:38:15 +0200

    Correct If statement in daily_reminder.py file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,23 +2,25 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
-match (priority, time_bound):
-    case ("high", "yes"):
-        print(
-            f"Reminder: '{task}' is a high priority and time-bound task that needs your attention!")
-    case ("high", "no"):
-        print(
-            f"Reminder: '{task}' is a high priority task. Please address it soon.")
-    case ("medium", "yes"):
-        print(
-            f"Reminder: '{task}' is a medium priority and time-bound task. Don't forget to complete it!")
-    case ("medium", "no"):
-        print(
-            f"Reminder: '{task}' is a medium priority task. Try to get to it when you can.")
+match (priority):
+    case ("high"):
+        if time_bound == "yes":
+            print(
+                f"Reminder: '{task}' is a {priority} priority and time-bound task that needs your attention!")
+        else:
+            print(
+                f"Note: '{task}' is a {priority} priority and time-bound task that needs your attention!")
+    case ("medium"):
+        if time_bound == "yes":
+            print(
+                f"Reminder: '{task}' is a {priority} priority and time-bound task. Don't forget to complete it!")
+        else:
+            print(
+                f"Note: '{task}' is a {priority} priority task. Try to get to it when you can.")
     case ("low"):
         if time_bound == "yes":
             print(
-                f"Note: '{task}' is a low priority but time-bound task. Schedule it accordingly.")
+                f"Reminder: '{task}' is a {priority} priority but time-bound task. Schedule it accordingly.")
         else:
             print(
-                f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
+                f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
