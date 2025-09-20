a359012814ad5be2b843817318199046cbe73cd1
Hasna_challa <challahasna@gmail.com>
2025-09-20 21:57:06 +0200

    Correct case condition value in daily_reminder.py file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,21 +3,21 @@ priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority:
-    case ("high"):
+    case "high":
         if time_bound == "yes":
             print(
                 f"Reminder: '{task}' is a {priority} priority and time-bound task that needs your attention!")
         else:
             print(
                 f"Note: '{task}' is a {priority} priority and time-bound task that needs your attention!")
-    case ("medium"):
+    case "medium":
         if time_bound == "yes":
             print(
                 f"Reminder: '{task}' is a {priority} priority and time-bound task. Don't forget to complete it!")
         else:
             print(
                 f"Note: '{task}' is a {priority} priority task. Try to get to it when you can.")
-    case ("low"):
+    case "low":
         if time_bound == "yes":
             print(
                 f"Reminder: '{task}' is a {priority} priority but time-bound task. Schedule it accordingly.")
