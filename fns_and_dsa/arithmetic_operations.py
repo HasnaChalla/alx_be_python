84afba465ed506c29b9b996ac792cd2e2f6feddf
Hshella <163134999+HasnaChalla@users.noreply.github.com>
2025-09-27 19:11:10 +0200

    Update arithmetic_operations.py
    remove arguments values fron the call of the function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,8 +1,7 @@
-def perform_operation(num1, num2, operation):
+def perform_operation():
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    operation = input(
-        "Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
     operations = {
         'add': lambda x, y: x + y,
         'subtract': lambda x, y: x - y,
@@ -10,9 +9,9 @@ def perform_operation(num1, num2, operation):
         'divide': lambda x, y: x / y if y != 0 else "Error: Division by zero is not allowed."
     }
     if operation in operations:
-        print(f"Result: ", operations[operation](num1, num2))
+        print(f"Result: {operations[operation](num1, num2)}")
     else:
         print("Error: Invalid operation.")
 
 
-perform_operation(5, 6, 'add')
+perform_operation()
