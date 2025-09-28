b6e45b7acdabec2003d788ef24b46f550d1240cd
Hasna_challa <challahasna@gmail.com>
2025-09-28 12:13:15 +0200

    correct the constants and fix functions return

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,22 +1,20 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
-CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 temperature = float(input("Enter the temperature to convert: "))
 temperature_unit = input(
     "Is this temperature in Celsius or Fahrenheit? (C/F):")
 
 
 def convert_to_celsius(fahrenheit):
-    global FAHRENHEIT_TO_CELSIUS_FACTOR
-    fahrenheit = (temperature - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
-    print(f"{temperature}°C is {fahrenheit}°F")
-    return fahrenheit
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    print(f"{fahrenheit}°F is {celsius}°C")
+    return celsius
 
 
 def convert_to_fahrenheit(celsius):
-    global CELSIUS_TO_FAHRENHEIT_FACTOR
-    celsius = temperature * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
-    print(f"{temperature}°F is {celsius}°C")
-    return celsius
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    print(f"{celsius}°C is {fahrenheit}°F")
+    return fahrenheit
 
 
 if temperature_unit.upper() == "C":
